25d188c2038b59208427c44e28eaa17f71a245df
makoto2025 <aratamakoto2025@gmail.com>
2026-05-01 19:18:47 -0700

    ex11

--- exercises/ex11.ts
+++ exercises/ex11.ts
@@ -1,5 +1,14 @@
 // 11.Exercise: Nullish Coalescing
 
 // Create a variable userInput with a null value.
-// Use nullish coalescing to set a default value Please enter something... if userInput is null or undefined.
+// Use nullish coalescing to set a default value Please enter something... 
+// if userInput is null or undefined.
 // Display the resulting value.
+
+
+const userInput = null;
+
+const storedData = userInput ?? "Please enter something...";
+
+
+console.log(storedData); 
